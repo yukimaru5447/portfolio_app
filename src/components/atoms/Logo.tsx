ed9eb32aadd7c7038387ade4d97f94d12e7df93d
yukimaru5447 <83536844+yukimaru5447@users.noreply.github.com>
2022-10-28 21:51:20 +0900

    Create Logo Component

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+import Link from 'next/link'
+
+export const Logo: FC = () => {
+  return (
+    <Link
+      href={'/'}
+      className="ml-4 p-4 text-2xl font-bold text-slate-500 hover:text-slate-700"
+    >
+      PortfolioApp
+    </Link>
+  )
+}
