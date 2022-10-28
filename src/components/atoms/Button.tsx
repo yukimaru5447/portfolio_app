1cea00205807d6ef3dcccc9a76b2ad68f10adffe
yukimaru5447 <83536844+yukimaru5447@users.noreply.github.com>
2022-10-28 21:50:53 +0900

    Create Button Component

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+
+type ButtonProps = {
+  name: string
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+  const { name } = props
+  return (
+    <button className="my-4 mr-6 rounded-md bg-slate-500 px-4 py-2 text-sm font-medium text-white hover:bg-slate-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-white">
+      {name}
+    </button>
+  )
+}
