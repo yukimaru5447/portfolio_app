816805c27e394a94a5487c15b2da03547ec178ef
yukimaru5447 <83536844+yukimaru5447@users.noreply.github.com>
2022-10-28 21:51:32 +0900

    Update post.tsx

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -5,6 +5,10 @@ import { Layout, Auth, Header } from '../components/_common/_index'
 import { ImageField } from '../components/atoms/ImageField'
 import { TextField } from '../components/atoms/TextField'
 import { TextArea } from '../components/atoms/TextArea'
+import { LinkBar } from '../components/atoms/LinkBar'
+
+import { Button } from '../components/atoms/Button'
+import { Logo } from '../components/atoms/Logo'
 
 const Post: NextPage = () => {
   const { data: session } = useSession()
@@ -19,11 +23,20 @@ const Post: NextPage = () => {
 
   return (
     <div>
-      <div className="h-14 bg-slate-100" />
-      <div className="flex flex-col items-center space-y-4 bg-slate-100">
+      <header className="flex justify-between bg-slate-100 md:flex">
+        <Logo />
+        <div className="flex justify-between">
+          <Button name="下書き保存" />
+          <Button name="ポートフォリオを投稿する" />
+        </div>
+      </header>
+
+      <div className="flex flex-col items-center space-y-4 bg-slate-100 pt-10 pb-20">
         <ImageField />
         <TextField />
         <TextArea />
+        <LinkBar name="URL" />
+        <LinkBar name="Github" />
       </div>
     </div>
   )
