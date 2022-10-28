821f9d65a22445aef8c2779ca0576b4f2b8e3255
kosuke-17 <tamutamu17@icloud.com>
2022-10-28 22:40:47 +0900

    lint修正

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,7 +4,6 @@ import GithubProvider from 'next-auth/providers/github'
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from '../../../server/db/client'
-import { env } from '../../../env/server.mjs'
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
