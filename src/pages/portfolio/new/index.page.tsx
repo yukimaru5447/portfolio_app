46eb1da9ea4bdb736898ad933b3f6149b3c50842
yukimaru5447 <83536844+yukimaru5447@users.noreply.github.com>
2022-10-29 10:24:21 +0900

    add post function

--- src/pages/portfolio/new/index.page.tsx
+++ src/pages/portfolio/new/index.page.tsx
@@ -1,16 +1,36 @@
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import { Layout, Auth } from '../../../components/_common/_index'
 import ImageField from '../../../components/organisms/ImageField'
 import { TextField } from '../../../components/atoms/TextField'
 import { TextArea } from '../../../components/atoms/TextArea'
 import { LinkBar } from '../../../components/atoms/LinkBar'
-
 import { Button } from '../../../components/atoms/Button'
 import { Logo } from '../../../components/atoms/Logo'
+import { FormEvent, useState } from 'react'
+import { useMutatePortfolio } from '../../../hooks/useMutatePortfolio'
 
 const Post: NextPage = () => {
+  const router = useRouter()
   const { data: session } = useSession()
+  const { postPortfolioMutation } = useMutatePortfolio()
+
+  const [title, setTitle] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+  const [serviceUrl, setServiceUrl] = useState<string>('')
+  const [githubUrl, setGithubUrl] = useState<string>('')
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    postPortfolioMutation.mutate({
+      title,
+      description,
+      serviceUrl,
+      githubUrl,
+    })
+    router.push('/')
+  }
 
   if (!session) {
     return (
@@ -21,7 +41,7 @@ const Post: NextPage = () => {
   }
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <header className="flex justify-between bg-slate-100 md:flex">
         <Logo />
         <div className="flex justify-between">
@@ -32,12 +52,23 @@ const Post: NextPage = () => {
 
       <div className="flex flex-col items-center space-y-4 bg-slate-100 pt-10 pb-20">
         <ImageField />
-        <TextField />
-        <TextArea />
-        <LinkBar name="URL" />
-        <LinkBar name="Github" />
+        <TextField value={title} onChange={(e) => setTitle(e.target.value)} />
+        <TextArea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <LinkBar
+          name="URL"
+          value={serviceUrl}
+          onChange={(e) => setServiceUrl(e.target.value)}
+        />
+        <LinkBar
+          name="Github"
+          value={githubUrl}
+          onChange={(e) => setGithubUrl(e.target.value)}
+        />
       </div>
-    </div>
+    </form>
   )
 }
 
