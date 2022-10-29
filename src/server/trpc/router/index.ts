c744bc5c3a47b1258b76f720eb8756325ab5a223
yukimaru5447 <83536844+yukimaru5447@users.noreply.github.com>
2022-10-29 10:23:04 +0900

    export portfolioRouter with appRouter

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,7 +1,10 @@
 // src/server/trpc/router/index.ts
-import { t } from "../trpc";
+import { t } from '../trpc'
+import { portfolioRouter } from './portfolio'
 
-export const appRouter = t.router({});
+export const appRouter = t.router({
+  portfolio: portfolioRouter,
+})
 
 // export type definition of API
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
