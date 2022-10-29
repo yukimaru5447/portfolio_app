4d8301fb9e77e77c27f6409ea29a3990e70c0986
yukimaru5447 <83536844+yukimaru5447@users.noreply.github.com>
2022-10-29 10:19:30 +0900

    Update TextField.tsx

--- src/components/atoms/TextField.tsx
+++ src/components/atoms/TextField.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react'
+import React, { FC, memo } from 'react'
 
-export const TextField: FC = () => {
+type Props = {
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const TextField: FC<Props> = ({ value, onChange }) => {
   return (
     <div className="w-2/3">
       <input
         type="text"
         className="w-full bg-slate-100 py-4 px-2 text-left text-3xl font-semibold focus:outline-none"
         placeholder="タイトル（32文字以内）"
+        value={value}
+        onChange={(e) => onChange(e)}
       />
     </div>
   )
