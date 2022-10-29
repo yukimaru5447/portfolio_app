3dd485915ba9dc58d986735e9ca4f3a7dd7cf3cd
yukimaru5447 <83536844+yukimaru5447@users.noreply.github.com>
2022-10-29 10:18:22 +0900

    Update LinkBar.tsx

--- src/components/atoms/LinkBar.tsx
+++ src/components/atoms/LinkBar.tsx
@@ -2,11 +2,11 @@ import React, { FC } from 'react'
 
 type Props = {
   name: string
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const LinkBar: FC<Props> = (props) => {
-  const { name } = props
-
+export const LinkBar: FC<Props> = ({ name, value, onChange }) => {
   return (
     <div className="flex w-2/3">
       <div className="text-ms flex h-12 w-28 items-center justify-center rounded-l-lg  bg-slate-300 text-center font-semibold text-white">
@@ -16,6 +16,8 @@ export const LinkBar: FC<Props> = (props) => {
         type="text"
         className="h-12 w-full rounded-r-lg bg-white px-4 text-xl focus:outline-none"
         placeholder="https://"
+        value={value}
+        onChange={(e) => onChange(e)}
       />
     </div>
   )
