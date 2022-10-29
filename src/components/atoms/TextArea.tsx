832804632ef0e4a824e6e671da0d065f0317d111
yukimaru5447 <83536844+yukimaru5447@users.noreply.github.com>
2022-10-29 10:18:39 +0900

    Update TextArea.tsx

--- src/components/atoms/TextArea.tsx
+++ src/components/atoms/TextArea.tsx
@@ -1,13 +1,20 @@
 import React, { FC } from 'react'
 import TextareaAutosize from 'react-textarea-autosize'
 
-export const TextArea: FC = () => {
+type Props = {
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+}
+
+export const TextArea: FC<Props> = ({ value, onChange }) => {
   return (
     <div className="w-2/3">
       <TextareaAutosize
         minRows={10}
         className="h-full w-full rounded-xl px-6 py-6 text-2xl font-medium focus:outline-none"
         placeholder="ポートフォリオの説明（マークダウン記法を使用できます）"
+        value={value}
+        onChange={(e) => onChange(e)}
       />
     </div>
   )
